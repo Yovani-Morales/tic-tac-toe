28c24ebb37428958eff5fa96d2275c27ba9f8658
Yovani Morales <83714102+Yovani-Morales@users.noreply.github.com>
2023-12-26 12:17:42 -0600

    add shifts

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,24 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
+  const [xIsNext, setxIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
+
     const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
+
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+
     setSquares(nextSquares);
+    setxIsNext(!xIsNext);
   }
 
   return (
