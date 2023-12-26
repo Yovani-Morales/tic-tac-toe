694761f4e387f022666453cf8a371980a61b8220
Yovani Morales <83714102+Yovani-Morales@users.noreply.github.com>
2023-12-25 22:09:15 -0600

    basic concepts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-import './App.css';
+import { useState } from 'react';
+import './style.css';
 
-function App() {
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+
   return (
-    <div></div>
+    <button onClick={handleClick} className='square'>{value}</button>
   );
 }
 
-export default App;
+export default function Board() {
+  return (
+    <>
+      <div className='board-row'>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className='board-row'>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className='board-row'>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  )
+}
